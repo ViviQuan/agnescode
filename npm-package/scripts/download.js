6e0c5f86415b450a8ca0f2fc5fdfabf665ff7734
ViviQuan <98718821@qq.com>
2026-06-30 19:38:36 +0800

    fix: consider providers with API key as connected

--- npm-package/scripts/download.js
+++ npm-package/scripts/download.js
@@ -10,7 +10,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const BIN_DIR = join(__dirname, "..", "bin")
 const BIN_PATH = join(BIN_DIR, "agnes")
 
-const VERSION = "0.1.1"
+const VERSION = "0.1.2"
 
 function getTarget() {
   const os = platform()
