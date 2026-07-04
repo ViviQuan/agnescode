b51f91b6610e4ad6bf880a41ef8f27a0f54a98cc
ViviQuan <98718821@qq.com>
2026-07-04 21:31:19 +0800

    chore: bump npm-package to v0.1.5 for release

--- npm-package/scripts/download.js
+++ npm-package/scripts/download.js
@@ -10,7 +10,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const BIN_DIR = join(__dirname, "..", "bin")
 const BIN_PATH = join(BIN_DIR, "agnes")
 
-const VERSION = "0.1.2"
+const VERSION = "0.1.5"
 
 function getTarget() {
   const os = platform()
